36b5da1c2191ec2a73cc02a4736f7d098dbd8cf7
Yelnur <ent.elnur.tur@gmail.com>
2024-07-29 20:29:03 +0500

    fixed

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,11 +1,12 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const token = useCookie("token");
   if (token.value) {
-    console.log("to", to);
-    console.log(from);
     if (to.name === "login" || to.name === "auth") {
       console.log("navigated to index page, authorized");
       return navigateTo({ name: "index" });
     }
+  }
+  else {
+    if(to.name !== 'login' && to.name !== 'auth') return navigateTo({ name: 'login'})
   }
 });
